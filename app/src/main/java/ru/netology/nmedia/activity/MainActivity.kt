da1a484ea2cba5da1b59387339e6d75a09c48c47
Anastasia <n41111215@mail.ru>
2021-12-30 14:18:23 +0300

    Домашнее задание к занятию «2.4. CRUD: списки, добавление, удаление, изменение»

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.adapter.onInteractionListener
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.utils.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +18,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter({
-            viewModel.likeById(it.id)
-        }, {
-            viewModel.shareById(it.id)
-            // логика обработки OnClickListener
+        val adapter = PostsAdapter(object : onInteractionListener {
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
         })
         binding.list.adapter = adapter
-        viewModel.data.observe(this) { posts ->
+        viewModel.data.observe(this, { posts ->
             adapter.submitList(posts)
+        })
+
+        viewModel.edited.observe(this, { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.content) {
+                requestFocus()
+                setText(post.content)
+            }
+        })
+
+        binding.save.setOnClickListener {
+            with(binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Content can't be empty",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+            }
         }
     }
-
 }
 
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 
 class PostRepositoryInMemoryImpl : PostRepository {
+    private var nextId = 1L
     private var posts = listOf(
         Post(
-            id = 1,
+            id = nextId++,
             author = "Нетология. Университет интернет-профессий",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим ,что в каждом уже есть сила , которая заставляет хотеть больше ,целиться выше , бежать быстрее. Наша миссия-помочь встать на пути роста и начать цепочку перемен > http://netolo.gy/fyb ",
             published = "21 мая в 18:36",
@@ -17,7 +18,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             likedByMe = false
         ), // create mock data
         Post(
-            id = 2,
+            id = nextId++,
             author = "Нетология. Университет интернет-профессий",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим ,что в каждом уже есть сила , которая заставляет хотеть больше ,целиться выше , бежать быстрее. Наша миссия-помочь встать на пути роста и начать цепочку перемен > http://netolo.gy/fyb ",
             published = "18 сентября в 10:12",
@@ -28,7 +29,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
 
         ),
         Post(
-            id = 3,
+            id = nextId++,
             author = "Нетология. Университет интернет-профессий",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим ,что в каждом уже есть сила , которая заставляет хотеть больше ,целиться выше , бежать быстрее. Наша миссия-помочь встать на пути роста и начать цепочку перемен > http://netolo.gy/fyb ",
             published = "19 сентября в 10:12",
@@ -39,7 +40,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
 
         ),
         Post(
-            id = 4,
+            id = nextId++,
             author = "Нетология. Университет интернет-профессий",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим ,что в каждом уже есть сила , которая заставляет хотеть больше ,целиться выше , бежать быстрее. Наша миссия-помочь встать на пути роста и начать цепочку перемен > http://netolo.gy/fyb ",
             published = "19 сентября в 10:12",
@@ -48,8 +49,8 @@ class PostRepositoryInMemoryImpl : PostRepository {
             views = 400,
             likedByMe = false
 
-        )
-    )
+        ),
+    ).reversed()
 
     private val data = MutableLiveData(posts) // создали оболочку из MutableLiveData
 
@@ -68,6 +69,31 @@ class PostRepositoryInMemoryImpl : PostRepository {
     override fun shareById(id: Long) {
         posts = posts.map {
             if (it.id != id) it else it.copy(share = it.share + 1)
+        }
+        data.value = posts
+    }
+
+    override fun removeById(id: Long) {
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+
+    override fun save(post: Post) {
+        if (post.id == 0L) {
+            posts = listOf(
+                post.copy(
+                    id = nextId++,
+                    author = "Me",
+                    likedByMe = false,
+                    published = "now"
+                )
+            ) + posts
+            data.value = posts
+            return
+        }
+
+        posts = posts.map {
+            if (it.id != post.id) it else it.copy(content = post.content)
         }
         data.value = posts
     }
