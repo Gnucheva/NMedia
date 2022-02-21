f1c6d67e72266857a44a62d81009447de2a7af4e
Anastasia <n41111215@mail.ru>
2022-02-21 13:20:36 +0300

    Домашнее задание к занятию «4.1 SQL и SQLite»

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -3,9 +3,10 @@ package ru.netology.nmedia.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryFileImpl
+import ru.netology.nmedia.repository.PostRepositorySQLiteImpl
 
 private val empty = Post(
     id = 0,
@@ -13,15 +14,16 @@ private val empty = Post(
     author = "",
     likedByMe = false,
     published = ""
-)
+    )
 
 class PostViewModel(application: Application) :
     AndroidViewModel(application) { // реализация хранения state(состояние данных)
     // упрощенный вариант
-    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    private val repository: PostRepository =
+        PostRepositorySQLiteImpl(AppDb.getInstance(application).postDao)
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
-    val currentPost= MutableLiveData(empty)
+    val currentPost = MutableLiveData(empty)
 
     fun save() {
         edited.value?.let {
@@ -45,7 +47,7 @@ class PostViewModel(application: Application) :
         }
     }
 
-    fun selectPost(post:Post){
+    fun selectPost(post: Post) {
         currentPost.value = post
     }
 
