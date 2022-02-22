68d1a70fa0bf2fd98e2385edfc945bf1cd7aa5b5
Anastasia <n41111215@mail.ru>
2022-02-22 11:47:08 +0300

    исправила Домашнее задание к занятию «Fragments, FragmentManager»

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,15 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long = 0,
+    val author: String = "",
+    val content: String = "",
+    val published: String = "",
+    val video: String = "",
+    val videoName: String = "",
+    val likes: Long = 0,
+    val share: Long = 0,
+    val views: Long = 0,
+    val likedByMe: Boolean = false
+
+)
